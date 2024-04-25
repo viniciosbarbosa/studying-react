0f13c2d1a2d46e7741da917c2c0f4542815dc501
viniciosl <viniciosbarbosagit@gmail.com>
2024-04-25 19:41:41 -0300

    modal criar post

--- tec-space/src/components/NavBar.tsx
+++ tec-space/src/components/NavBar.tsx
@@ -1,8 +1,17 @@
 import { signOut } from "firebase/auth";
 import { toast } from "react-toastify";
 import { auth } from "../firebase/FirebaseConection";
+import { useState, useRef, Fragment } from "react";
+import { Transition, Dialog } from "@headlessui/react";
 
 const NavBar = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [isNewPostFormValid, setIsNewPostFormValid] = useState(true);
+  const [progress, setProgress] = useState(0);
+  const [imgUrl, setImgUrl] = useState("");
+  const canceButtonrRef = useRef(null);
+
   const handleSignOut = async () => {
     await signOut(auth)
       .then(() => {
@@ -14,7 +23,6 @@ const NavBar = () => {
   return (
     <>
       {/*NAVBAR */}
-
       <div className="w-full mx-auto flex flex-wrap gap-5 p-5 flex-col md:flex-row items-center bg-purple-600">
         <button
           onClick={handleSignOut}
@@ -43,6 +51,7 @@ const NavBar = () => {
         </div>
 
         <button
+          onClick={() => setOpenModal(!openModal)}
           type="button"
           className="inline-flex items-center rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-600 focus-visible:outline-none focus-visible:outline-offset-2 focus-visible:outline-orange-600 border-0 mt-4 md:mt-0"
         >
@@ -63,6 +72,126 @@ const NavBar = () => {
           Criar Post
         </button>
       </div>
+
+      {/*MODAL DE CRIAR POSTS */}
+      <Transition.Root show={openModal} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relativo z-10"
+          initialFocus={canceButtonrRef}
+          onClose={setOpenModal}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="flex flex-col justify-center h-min"></div>
+          </Transition.Child>
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel>
+                  <div className="flex flex-col justify-center h-min">
+                    <form className="max-w-[400px] w-full mx-auto bg-purple-600 p-8 px-8 rounded-lg mt-10">
+                      <h2 className="text-3xl mb-5 dark:text-white font-bold text-center">
+                        Criar Post
+                      </h2>
+                      <div className="flex flex-col text-start text-white py-2">
+                        <label>Autor</label>
+                        <input
+                          type="text"
+                          placeholder="Digite o seu nome"
+                          className={`w-full rounded-lg mt-2 p-2 ${
+                            isNewPostFormValid
+                              ? `bg-purple-700 focus:bg-purple-800 `
+                              : `bg-red-700  focus:bg-red-800`
+                          }  bg-purple-700 focus:bg-purple-800 border-2 border-purple-800 focus:border-orange-700 focus:outline-none focus:placeholder-transparent`}
+                        />
+                      </div>
+
+                      <div className="flex flex-col text-start text-white py-2">
+                        <label>Título</label>
+                        <input
+                          type="text"
+                          placeholder="Digite o título"
+                          className={`w-full rounded-lg mt-2 p-2 ${
+                            isNewPostFormValid
+                              ? `bg-purple-700 focus:bg-purple-800 `
+                              : `bg-red-700  focus:bg-red-800`
+                          }  bg-purple-700 focus:bg-purple-800 border-2 border-purple-800 focus:border-orange-700 focus:outline-none focus:placeholder-transparent`}
+                        />
+                      </div>
+
+                      <div className="flex flex-col text-start text-white py-2">
+                        <label>Conteúdo</label>
+                        <textarea
+                          placeholder="Digite o conteúdo"
+                          className={`w-full rounded-lg mt-2 p-2 ${
+                            isNewPostFormValid
+                              ? `bg-purple-700 focus:bg-purple-800 `
+                              : `bg-red-700  focus:bg-red-800`
+                          }  bg-purple-700 focus:bg-purple-800 border-2 border-purple-800 focus:border-orange-700 focus:outline-none focus:placeholder-transparent`}
+                        />
+                      </div>
+
+                      <div className="flex flex-col text-start text-white py-2">
+                        <label>Capa</label>
+                        <input
+                          type="file"
+                          placeholder="Digite o conteúdo"
+                          className="w-full cursor-pointer rounded-lg mt-2 p-2 bg-purple-700 focus:bg-purple-800 border-2 border-purple-800 focus:border-orange-700 focus:outline-none focus:placeholder-transparent"
+                        />
+                      </div>
+
+                      {!imgUrl && isLoading && (
+                        <progress value={progress} max="100" />
+                      )}
+
+                      {imgUrl && !isLoading && (
+                        <img
+                          src={imgUrl}
+                          alt="Imagem de capa do post"
+                          width={200}
+                        />
+                      )}
+
+                      <button
+                        disabled={isLoading}
+                        type="submit"
+                        className="w-full my-5 py-2 bg-orange-500 shadow-lg  enabled:hover:shadow-orange-500/40 text-white font-semibold rounded-lg disabled:bg-orange-400 disabled:shadow-none enabled:shadow-orange-500/50"
+                      >
+                        {isLoading ? "Criando Post" : "Criar"}
+                      </button>
+
+                      <button
+                        onClick={() => setOpenModal(false)}
+                        disabled={isLoading}
+                        type="button"
+                        className="w-full py-2 bg-red-500 shadow-lg  enabled:hover:shadow-red-500/40 text-white font-semibold rounded-lg disabled:bg-red-400 disabled:shadow-none enabled:shadow-red-500/50"
+                      >
+                        Cancelar
+                      </button>
+                    </form>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 };
