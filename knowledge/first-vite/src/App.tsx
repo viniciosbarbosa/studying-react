1e4e2bd5b0cc9111c40faccdbec8096ec2ee4512
viniciosl <viniciosbarbosagit@gmail.com>
2025-02-13 00:53:54 -0300

    react

---
+++ knowledge/first-vite/src/App.tsx
@@ -0,0 +1,13 @@
+import { UserEmail, UserName } from "./UserName";
+
+const App = () => {
+  return (
+    <div>
+      component
+      <UserName />
+      <UserEmail />
+    </div>
+  );
+};
+
+export default App;
---
+++ knowledge/first-vite/src/UserName.tsx
@@ -0,0 +1,9 @@
+const UserName = () => {
+  return <h4>teste</h4>;
+};
+
+const UserEmail = () => {
+  return <h4>teste 2</h4>;
+};
+
+export { UserName, UserEmail };
