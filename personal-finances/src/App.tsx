efe128b254f2bbc95799db3e24c050de15ddc551
vinicios <viniciosbarbosagit@gmail.com>
2025-06-19 17:57:59 -0300

    hooks useTheme

--- personal-finances/src/App.tsx
+++ personal-finances/src/App.tsx
@@ -1,14 +1,18 @@
 import { useEffect } from "react";
 import { useAuth } from "./hooks/auth";
+import { useTheme } from "./hooks/theme";
 
-const app = () => {
+const App = () => {
   const { handleAuthenticateUser } = useAuth();
+  const { handleInitTheme } = useTheme();
 
   useEffect(() => {
     handleAuthenticateUser();
+
+    handleInitTheme();
   }, []);
 
   return <div>...</div>;
 };
 
-export default app;
+export default App;
